5c9cea4ffa62adab19f58797bcc967336a30081c
Damian Mroczek <damianmroczek@yahoo.com>
2023-01-06 17:35:31 +0700

    events, if status.id

--- script.js
+++ script.js
@@ -12,16 +12,47 @@ const API_Key = '&appid=29ad3c3079f849423de84ab9851783ce'
 const API_UNITS = '&units=metric'
 
 const getWeather = () => {
-    const city = input.value || 'London'
+    const city = input.value || 'Bangkok'
     const URL = API_LINK + city + API_Key + API_UNITS
     axios.get(URL).then(res => {
 
         const temp = res.data.main.temp
-        const humidity = res.data.main.humidity
+        const hum = res.data.main.humidity
+        const status = Object.assign({}, ...res.data.weather)
 
         cityName.textContent = res.data.name
         temperature.textContent = Math.floor(temp) + '°C'
-    })
+        humidity.textContent = hum + '%'
+        weather.textContent = status.main
+        warning.textContent = ''
+        input.value = ''
+
+        if (status.id >= 200 && status.id < 300) {
+            photo.setAttribute('src', './img/thunderstorm.png')
+        } else if (status.id >= 300 && status.id < 400) {
+            photo.setAttribute('src', './img/drizzle.png')
+        } else if (status.id >= 500 && status.id < 600) {
+            photo.setAttribute('src', './img/rain.png')
+        } else if (status.id >= 600 && status.id < 700) {
+            photo.setAttribute('src', './img/ice.png')
+        } else if (status.id >= 700 && status.id < 800) {
+            photo.setAttribute('src', './img/fog.png')
+        } else if (status.id === 800) {
+            photo.setAttribute('src', './img/sun.png')
+        } else if (status.id >= 800 && status.id < 900) {
+            photo.setAttribute('src', './img/cloud.png')
+        } else {
+            photo.setAttribute('src', './img/unknown.png')
+        }
+    }).catch(() => warning.textContent = 'Wpisz poprawną nazwę miasta!')
 }
 
+const enterCheck = (e) => {
+    if (e.key === 'Enter') {
+        getWeather()
+    }
+}
+
+input.addEventListener('keyup', enterCheck)
 getWeather()
+button.addEventListener('click', getWeather)
